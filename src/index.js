d4378e5f63e0dc4fa38a433d400a819bc11e7c27
kevinsangnguyen <kevinsangnguyen@gmail.com>
2017-07-26 18:49:06 -0700

    finished PostIndex

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
 import reducers from './reducers';
-import PostsIndex from './components/posts_index'
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
         <Route path="/" component={PostsIndex} />
+        <Route path="/posts/new" component={PostsNew} />
       </div>
     </BrowserRouter>
   </Provider>

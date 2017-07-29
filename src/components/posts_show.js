31aca51b61e3fe6487cb83f37dd952675abe9873
kevinsangnguyen <kevinsangnguyen@gmail.com>
2017-07-28 19:21:18 -0700

    show post page

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { fetchPost } from '../actions';
+import { connect } from 'react-redux';
 
 class PostsShow extends Component {
+  componentDidMount() {
+    const  { id } = this.props.match.params.id;
+    this.props.fetchPost(id);
+  }
   render() {
     return (
       <div>
@@ -10,4 +16,8 @@ class PostsShow extends Component {
   }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+  return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })( PostsShow );
